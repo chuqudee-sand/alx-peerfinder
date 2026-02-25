0c33b706212cf88f426a6eb0cfafb7ab7d20f830
chuqudee-sand <cokereafor@sandtech.com>
2026-02-25 06:42:01 +0100

    Update LandingPage.jsx with latest walkthrough video

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -101,7 +101,7 @@ const LandingPage = () => {
             </div>
           </motion.div>
           <motion.div initial={{ opacity: 0, x: 50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8, delay: 0.2 }} style={styles.videoWrapper}>
-            <iframe src="https://www.youtube.com/embed/QfHu33-2aR0" title="PeerFinder Walkthrough" style={styles.iframe} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+            <iframe src="https://www.youtube.com/embed/78uUL2WFo0k" title="PeerFinder Walkthrough" style={styles.iframe} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
           </motion.div>
         </div>
       </div>
@@ -256,3 +256,4 @@ const styles = {
 };
 
 export default LandingPage;
+
