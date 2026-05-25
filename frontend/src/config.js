4b41866f9af733d0e36194e787123134753b0cb7
chuqudee-sand <cokereafor@sandtech.com>
2026-05-25 11:43:37 +0100

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -6,6 +6,6 @@ const isProduction = window.location.hostname !== 'localhost' && window.location
 // 2. Set the API URL accordingly
 // REPLACE the string below with your actual Render Backend URL once you have it
 export const API_URL = isProduction 
-  ? 'https://alx-foundations-peer-finder.onrender.com' 
+  ? 'https://alx-foundations-peer-finder.onrrrrrender.com' 
 
   : 'http://127.0.0.1:5000';
