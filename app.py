2ef65ff8a320040fb8c1b3d9c9123b8ae6f8741f
chuqudee-sand <cokereafor@sandtech.com>
2026-05-26 07:16:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ SESSION_FEEDBACK_OBJECT_KEY = 'alx-master-session_feedback.csv'
 NO_SHOW_OBJECT_KEY = 'alx-master-no_show.csv'  
 UNPAIR_REASONS_KEY = 'alx-master-unpair_reasons.csv'    
 
-ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', 'admin123')
+ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
 
 def load_google_token(env_var_name):
     token_str = os.environ.get(env_var_name)
